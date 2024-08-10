9752145db8ba4da9b8a19f8e91c715f8af89e327
Matthew Valaitis <matthew.w.valaitis@live.co.uk>
2024-08-10 07:44:38 +0100

    First commit of the FelineFinder app

---
+++ FelineFinderApp/FelineFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FelineFinderAppApp.swift
+//  FelineFinderApp
+//
+//  Created by Matthew Valaitis on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FelineFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
